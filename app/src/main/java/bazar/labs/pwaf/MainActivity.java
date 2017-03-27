951534f1799c2826e9fed76d91e3bc654cdbae7a
윤홍일 <hiyoon@yunhong-il-ui-iMac.local>
2017-03-28 01:53:04 +0900

    Initial commit

---
+++ app/src/main/java/bazar/labs/pwaf/MainActivity.java
@@ -0,0 +1,13 @@
+package bazar.labs.pwaf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/bazar/labs/pwaf/SplashActivity.java
@@ -0,0 +1,33 @@
+package bazar.labs.pwaf;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // set splash thread
+        Thread splashThread = new Thread(){
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        // strat thread
+        splashThread.start();
+    }
+}
